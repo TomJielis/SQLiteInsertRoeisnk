41c6b2dd7e6ab77d9303460620108e19f02232de
tomjielis <tomvanwijgerden@gmail.com>
2017-10-25 16:46:04 +0200

    Fixed bugs

---
+++ app/src/main/java/com/example/sqliteinsert/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.sqliteinsert;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText Name, Pass;
+    myDbAdapter helper;
+    public static Context context;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        context = getApplicationContext();
+        Name= (EditText) findViewById(R.id.editText);
+        Pass= (EditText) findViewById(R.id.editText2);
+        helper = new myDbAdapter(this);
+
+        Button adduser =(Button)findViewById(R.id.btnAddUser);
+
+        adduser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addUser(v);
+            }
+        });
+    }
+
+    public void addUser(View view)
+    {
+       // Toast.makeText(this,"Running", Toast.LENGTH_LONG).show();
+        String t1 = Name.getText().toString();
+        String t2 = Pass.getText().toString();
+        long identity = helper.insertData(t1,t2);
+        if(identity<0)
+        {
+            Message.message(context,"Unsuccessful");
+        } else
+        {
+            Message.message(context,"Successful");
+        }
+    }
+}
